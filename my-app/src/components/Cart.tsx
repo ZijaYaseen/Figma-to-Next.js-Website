f8fa3eb643280128fbd074265b29c6dade34a8a4
Zija Yaseen <zijayaseen15@gmail.com>
2024-12-14 02:45:28 +0500

    update

--- my-app/src/components/Cart.tsx
+++ my-app/src/components/Cart.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 import { RiArrowRightSLine } from "react-icons/ri";
 
 const Cart = () => {
-  return (
-    <div className='max-w-[1440vw] font-poppins w-full md:mt-[90px] mt-[60px]' >
+    return (
+        <div className='max-w-[1440vw] font-poppins w-full md:mt-[90px] mt-[60px]' >
 
             {/* Cart 1st section */}
 
@@ -32,8 +32,59 @@ const Cart = () => {
                     />
                     <h1 className='font-light'>Cart</h1>
                 </div>
-            </div></div>
-  )
+            </div>
+            {/* second section */}
+            <div className='md:py-10 py-5 gap-5 md:w-[90%] w-[95%] flex flex-col md:flex-row justify-between mx-auto'>
+
+                <div className='flex md:flex-col flex-row gap-8 w-[60%]'>
+
+                    <div className='flex md:flex-row flex-col items-center gap-5 md:gap-0 py-4 bg-[#FFF9E5] font-medium text-base md:pl-[90px] p-5'>
+                        <p className='md:w-[300px]'>Product</p>
+                        <p className='md:w-[300px]'>Price</p>
+                        <p className='md:w-[200px]'>Quantity</p>
+                        <p className='md:w-[300px]'>Subtotal</p>
+                    </div>
+
+                    <div className='flex gap-1 md:items-center'>
+                        <Image src={"/CartAsgaardsofa4.svg"} alt='Cart' width={100} height={100}
+                            className="bg-[#FBEBB5] hidden md:block w-[76px] md:h-[80px] h-[20px] md:rounded-[10px] rounded-sm">
+
+                        </Image>
+                        <div className='flex font-normal text-base text-[#9F9F9F] md:flex-row flex-col gap-5 md:gap-0 p-3'>
+                            <p className='w-[180px]'>Asgaard sofa</p>
+                            <p className='w-[190px]'>Rs. 250,000.00</p>
+                            <p className='border border-[#9F9F9F] w-8 h-8 rounded-[5px] flex items-center justify-center text-black'>1</p>
+                            <p className='w-[150px] md:ml-[76px] text-black'>Rs. 250,000.00</p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className='md:w-[393px] bg-[#FFF9E5] md:h-[390px] h-[290px] flex flex-col items-center'>
+                    <h1 className='mt-4 font-semibold text-[32px]'>Cart Totals</h1>
+
+                    <div className='flex justify-between md:p-16 p-5 w-[95%]'>
+                        <div className='flex flex-col gap-10 font-medium text-base'>
+                            <p>Subtotal</p>
+                            <p>Total</p>
+                        </div>
+
+                        <div className='flex flex-col gap-10 font-medium text-base text-right'>
+                            <p className='text-base font-normal text-[#9F9F9F]'>Rs. 250,000.00</p>
+                            <p className='text-xl font-medium text-[#B88E2F]'>Rs. 250,000.00</p>
+                        </div>
+                    </div>
+
+                    <button className='flex justify-center mx-auto rounded-[15px] font-normal text-xl w-[222px] py-5 border border-black'>
+                        Check Out
+                    </button>
+                </div>
+
+
+            </div>
+
+        </div>
+    )
 }
 
 export default Cart
--- my-app/src/components/SubFooter.tsx
+++ my-app/src/components/SubFooter.tsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 const SubFooter = () => {
   return (
-    <div className='flex flex-col md:flex-row items-center md:justify-between justify-center py-5 bg-[#FAF4F4] md:h-[300px] md:px-28 px-10 gap-20'>
+    <div className='flex flex-col md:flex-row items-center md:justify-between justify-center py-7 bg-[#FAF4F4] md:h-[300px] md:px-28 px-5 md:gap-20 gap-7'>
 
           <div className='flex flex-col'>
-            <h2 className='font-medium text-[32px]'>Free Delivery</h2>
-            <p className='text-[#9F9F9F] font-normal text-xl'>For all oders over $50, consectetur adipim scing elit.</p>
+            <h2 className='font-medium md:text-[32px] text-xl'>Free Delivery</h2>
+            <p className='text-[#9F9F9F] font-normal md:text-xl text-base'>For all oders over $50, consectetur adipim scing elit.</p>
           </div>
 
           <div>
-            <h2 className='font-medium text-[32px]'>90 Days Return</h2>
-            <p className='text-[#9F9F9F] font-normal text-xl'>If goods have problems, consectetur adipim scing elit.</p>
+            <h2 className='font-medium md:text-[32px] text-xl'>90 Days Return</h2>
+            <p className='text-[#9F9F9F] font-normal md:text-xl text-base'>If goods have problems, consectetur adipim scing elit.</p>
           </div>
 
           <div>
-            <h2 className='font-medium text-[32px]'>Secure Payment</h2>
-            <p className='text-[#9F9F9F] font-normal text-xl'>100% secure payment, consectetur adipim scing elit.</p>
+            <h2 className='font-medium md:text-[32px] text-xl'>Secure Payment</h2>
+            <p className='text-[#9F9F9F] font-normal md:text-xl text-base'>100% secure payment, consectetur adipim scing elit.</p>
           </div>
 
         </div>
